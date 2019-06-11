bc1fe15a8c9b3c4e4f775d45b4e5ac429816024c
XrizA <25199860+XrizA@users.noreply.github.com>
2019-06-11 20:53:05 +0800

    Update Linefollower.c

--- EV3_Linefollower/Linefollower.c
+++ EV3_Linefollower/Linefollower.c
@@ -1,43 +1,30 @@
-#pragma config(Sensor, S3,     colour,         sensorEV3_Color)
-//#pragma config(Sensor, S1,     LeftLight,      sensorEV3_Color)
-//#pragma config(Sensor, S2,     RightLight,     sensorEV3_Color)
+#pragma config(Motor,  motorB,          leftWheel,     tmotorEV3_Large, PIDControl, driveLeft, encoder)
+#pragma config(Motor,  motorC,          rightWheel,    tmotorEV3_Large, PIDControl, driveRight, encoder)
 
-#pragma config(Motor,  motorB,          leftwheel,     tmotorEV3_Large, PIDControl, encoder)
-#pragma config(Motor,  motorC,          rightwheel,    tmotorEV3_Large, PIDControl, encoder)
-
-int nMotorSpeedSetting = 30;
-float nPfactor = 0.3;
-int grey = 50;
-int lowest = 100;
-int highest = 0;
-
-void scanLine()
-{
-	motor[leftwheel] = 10;
-	motor[rightwheel] = -10;
-
-	time1[T1] = 0;
-	while (time1[T1] < 500)
-	{
-		if (SensorValue[colour] > highest)
-			highest = SensorValue[colour]
-		if (SensorValue[colour] < lowest)
-			lowest = SensorValue[colour]
-	}
-	grey = (highest - lowest) / 2;
-	motor[leftwheel] = 0;
-	motor[rightwheel] = 0;
-}
+#define PWR 55
+#define NPWR -65
+#define THRESHOLD 35
+#define GOSTRAIGHT 50
 
 task main()
 {
-  float error;
-  scanLine();
-  while (true)
-  {
-    error = SensorValue[colour] - grey;
-    motor[leftwheel] = nMotorSpeedSetting - round(error * nPfactor);
-    motor[rightwheel] = nMotorSpeedSetting + round(error * nPfactor);
-    wait1Msec(50);
-  }
+	while (true)
+	{
+		// 2 white 3 white -> Go straight
+		if (getColorReflected(S2) > THRESHOLD && getColorReflected(S3) < THRESHOLD)
+		{
+			motor[leftWheel] = NPWR;
+			motor[rightWheel] = PWR;
+		}
+		else if (getColorReflected(S2) < THRESHOLD && getColorReflected(S3) > THRESHOLD)
+		{
+			motor[leftWheel] = PWR;
+			motor[rightWheel] = NPWR;
+		}
+		else
+		{
+			motor[leftWheel] = GOSTRAIGHT;
+			motor[rightWheel] = GOSTRAIGHT;
+		}
+	}
 }
