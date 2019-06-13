48c38056a93e96d24d38d26a31a890e464552417
XrizA <25199860+XrizA@users.noreply.github.com>
2019-06-13 16:06:56 +0800

    Update Linefollower1.c

--- EV3_Linefollower/Linefollower1.c
+++ EV3_Linefollower/Linefollower1.c
@@ -1,23 +1,15 @@
 #pragma config(Motor, motorB, leftWheel, tmotorEV3_Large, PIDControl, driveLeft, encoder)
 #pragma config(Motor, motorC, rightWheel, tmotorEV3_Large, PIDControl, driveRight, encoder)
 
-#define THRESHOLD 50
+#define THRESHOLD 35
 
 task main()
 {
 	while (true)
 	{
-		setMotorSync(leftWheel, rightWheel, 20, 45);
-		if (getColorReflected(S2) > THRESHOLD)
-		{
-			motor[leftWheel] = 0;
-			motor[rightWheel] = 50;
-		}
-		else
-		{
-			motor[leftWheel] = 50;
-			motor[rightWheel] = 0;
-		}
-		wait1Msec(1000);
+
+		if (getColorReflected(S2) > THRESHOLD) setMotorSync(leftWheel, rightWheel, -55, 100);
+		else if (getColorReflected(S3) > THRESHOLD) setMotorSync(leftWheel, rightWheel, 55, 100);
+		else setMotorSync(leftWheel, rightWheel, 0, 35);
 	}
 }
